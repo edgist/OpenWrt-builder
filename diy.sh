3e525de553b1b6de0b3f96b6e13cbbf11e90e9b9
KomoreKalu <34343603+KomoreKalu@users.noreply.github.com>
2020-03-08 16:36:46 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,7 +8,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/vernesong/OpenClash package/OpenClash
+#git clone https://github.com/vernesong/OpenClash package/OpenClash
 
 ####
 
